002b63fc1a042847bedd86d155087b18afe34de7
tangbiao <scxiaoshe@qq.com>
2022-01-31 15:23:11 +0800

    1.提高依赖包版本升级为最新修复log日志漏洞风险 2.日志输出格式配置化 io.github.clockworm.middle.logback.log-pattern

--- src/main/java/io/github/clockworm/appender/KafkaAppender.java
+++ src/main/java/io/github/clockworm/appender/KafkaAppender.java
@@ -1,10 +1,10 @@
 package io.github.clockworm.appender;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.Layout;
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
@@ -14,15 +14,15 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
 
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.AppenderBase;
-import ch.qos.logback.core.Layout;
-import lombok.Getter;
-import lombok.Setter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Setter
 @Getter
 public class KafkaAppender extends AppenderBase<ILoggingEvent> {
+	private static final String ERROR_SERVERS = "servers_IS_UNDEFINED";
 	private String topic;
 	private String servers;
 	private String acks;
@@ -30,27 +30,28 @@ public class KafkaAppender extends AppenderBase<ILoggingEvent> {
 	private int batchSize;
 	private int linger;
 	private int bufferMemory;
-	/**是否开启KAFKA发送日志 注意当true时需要结合LOGBACK配置名为kafkaLogger的<logger>...</logger>标签*/
+	/**
+	 * 是否开启KAFKA发送日志 注意当true时需要结合LOGBACK配置名为kafkaLogger的<logger>...</logger>标签
+	 */
 	private boolean enabledLog = false;
-	/**logger标签的名称*/
+	/**
+	 * logger标签的名称
+	 */
 	private String kafkaLogger;
-	/**日志输出组件*/
+	/**
+	 * 日志输出组件
+	 */
 	private Layout<ILoggingEvent> layout;
-
 	private KafkaTemplate<String, String> logProducer;
-
 	private ExecutorService kafkaPool = Executors.newSingleThreadExecutor();
-
-	private static final String  ERROR_SERVERS = "servers_IS_UNDEFINED";
-
-	private Logger kafkaLog  = null;
-
+	private Logger kafkaLog = null;
+	
 	@Override
 	public void start() {
-		if(enabledLog) {
+		if (enabledLog) {
 			kafkaLog = LoggerFactory.getLogger(kafkaLogger);
 		}
-		if(!ERROR_SERVERS.equals(servers) && logProducer==null) {
+		if (!ERROR_SERVERS.equals(servers) && logProducer == null) {
 			Map<String, Object> props = new HashMap<>();
 			props.put(ProducerConfig.ACKS_CONFIG, acks);
 			props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
@@ -60,17 +61,17 @@ public class KafkaAppender extends AppenderBase<ILoggingEvent> {
 			props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, bufferMemory);
 			props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 			props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-			DefaultKafkaProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(props,new StringSerializer(), new StringSerializer());
+			DefaultKafkaProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(props, new StringSerializer(), new StringSerializer());
 			this.logProducer = new KafkaTemplate<>(producerFactory);
 		}
 		super.start();
 	}
-
+	
 	@Override
 	public void stop() {
 		super.stop();
 	}
-
+	
 	@Override
 	protected void append(ILoggingEvent event) {
 		String level = event.getLevel().levelStr;
@@ -78,26 +79,29 @@ public class KafkaAppender extends AppenderBase<ILoggingEvent> {
 		try {
 			this.send(topic.toLowerCase(), level, doLayout);
 		} catch (Exception e) {
-			if(enabledLog) {
-				kafkaLog.error("Kafka日志组件,发送日志数据异常:{}",e);
+			if (enabledLog) {
+				kafkaLog.error("Kafka日志组件,发送日志数据异常:", e);
 			}
 		}
-
+		
 	}
-
-	/** 日志数据发送方法 */
-	private void send(String topic, String key, String message){
-		if(logProducer==null) return;
-		kafkaPool.execute(new Runnable(){
+	
+	/**
+	 * 日志数据发送方法
+	 */
+	private void send(String topic, String key, String message) {
+		if (logProducer == null) return;
+		kafkaPool.execute(new Runnable() {
 			@Override
 			public void run() {
 				ListenableFuture<SendResult<String, String>> future = logProducer.send(topic, key, message);
-				if(enabledLog) {
-					future.addCallback(o -> kafkaLog.info("send-日志数据发送成功 topic:{},partition:{},offset:{}",o.getRecordMetadata().topic(),o.getRecordMetadata().partition(),o.getRecordMetadata().offset()),
+				if (enabledLog) {
+					future.addCallback(
+							o -> kafkaLog.info("send-日志数据发送成功 topic:{},partition:{},offset:{}", o.getRecordMetadata().topic(), o.getRecordMetadata().partition(), o.getRecordMetadata().offset()),
 							throwable -> kafkaLog.error("send-日志数据发送失败 topic:{},log:{}", topic, message));
 				}
 			}
 		});
 	}
-
+	
 }
--- src/main/java/io/github/clockworm/properties/LogBackProperties.java
+++ src/main/java/io/github/clockworm/properties/LogBackProperties.java
@@ -1,15 +1,20 @@
 package io.github.clockworm.properties;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
-import lombok.Data;
 
 @ConfigurationProperties(prefix = "io.github.clockworm.middle.logback")
 @Component
 @Data
 public class LogBackProperties {
+	/**kafka地址 多个逗号隔开*/
 	private String kafkaServers;
-	private String projectAppName;
+	/**项目组名称*/
 	private String projectGroupName;
+	/**项目名称*/
+	private String projectAppName;
+	/**推送日志格式*/
+	private String logPattern;
 }
